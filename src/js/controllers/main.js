38b16bb908b208b31619122d3d0f11d371221632
Grace Lee <eunheh@gmail.com>
2016-11-14 15:36:43 -0500

    toggle works

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -5,15 +5,28 @@ function MainController ($scope) {
   $scope.number = 1;
   $scope.likesMsg = `0 Likes`;
 
-  $scope.addLike = function (newNumber) {
-    var newNumber = $scope.number++;
-    console.log(newNumber)
-    if (newNumber === 1) {
-      $scope.likesMsg = `${newNumber} Like`;
-    } else {
-      $scope.likesMsg = `${newNumber} Likes`;
-    }
+  var add = true;
+  $scope.toggle = function (change) {
+    add = !add;
   }
+
+    $scope.addLike = function () {
+      if (add === true) {
+        var newNumber = ++$scope.number;
+        if (newNumber === 1) {
+          $scope.likesMsg = `${newNumber} Like`;
+        } else {
+          $scope.likesMsg = `${newNumber} Likes`;
+        }
+      } else {
+        var newNumber = --$scope.number;
+        if (newNumber === 1) {
+          $scope.likesMsg = `${newNumber} Like`;
+        } else {
+          $scope.likesMsg = `${newNumber} Likes`;
+        }
+      }
+    }
 
 };
 
