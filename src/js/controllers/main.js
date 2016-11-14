21ab7a88633320f9b5073bb639aafad2a85c25a3
Grace Lee <eunheh@gmail.com>
2016-11-14 15:14:12 -0500

    likes buttom with like at 1

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,9 +1,19 @@
+import $ from 'jquery';
+
 function MainController ($scope) {
 
-  $scope.number = 0;
-  $scope.addLike = function () {
-    $scope.number = $scope.number + 1;
-  };
+  $scope.number = 1;
+  $scope.likesMsg = `0 Likes`;
+
+  $scope.addLike = function (newNumber) {
+    var newNumber = $scope.number++;
+    console.log(newNumber)
+    if (newNumber === 1) {
+      $scope.likesMsg = `${newNumber} Like`;
+    } else {
+      $scope.likesMsg = `${newNumber} Likes`;
+    }
+  }
 
 };
 
